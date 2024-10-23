408dba47a29eb8a82fb2cd634df72f2ec2b718bd
FIRST_NAME LAST_NAME <you@example.com>
2024-10-23 15:40:34 +0200

    PokemonCard

---
+++ my-react-app/src/components/PokemonCard.tsx
@@ -0,0 +1,17 @@
+function PokemonCard() {
+  return (
+    <>
+      <div>
+        <figure>
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+            alt=""
+          />
+        </figure>
+        <figcaption>Bulbasaur</figcaption>
+      </div>
+    </>
+  );
+}
+
+export default PokemonCard;
